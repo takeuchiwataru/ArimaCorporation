6e439c36518f13426f40290324234a14a9bdaf9e
eisuke1222 <eisuke1222z3@gmail.com>
2019-11-14 15:59:42 +0900

    ミニマップ、タイトルマップ

---
+++ BaseProject/ChickenRace/ChickenRace/title.cpp
@@ -0,0 +1,519 @@
+//=============================================================================
+//
+// タイトルの処理 [title.cpp]
+// Author : 有馬 武志
+//
+//=============================================================================
+#define _CRT_SECURE_NO_WARNINGS	//Fire Wall突破
+#include <string.h>				//ストリング使用のため
+#include "title.h"
+#include "main.h"
+#include "renderer.h"
+#include "manager.h"
+#include "input.h"
+#include "fade.h"
+#include "sound.h"
+#include "ranking.h"
+#include "ui.h"
+#include "titlecamera.h"
+#include "object.h"
+
+#include "titlemenu.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define EFFECT_FRAME	(300)		// 季節のエフェクトフレーム
+#define SEASON_MAX		(4)			// 季節のカウント
+#define EFFECT_CREATE	(10)		// エフェクト生成時間
+#define EFFECT_ROS		(D3DXVECTOR3(rand() % 1200 + 10.0f, -20.0f, 0.0f))	// 位置(ランダム)
+#define EFFECT_SIZE		(rand() % RADIUS_CONDIUIONS + 10)					// サイズ(ランダム)
+#define TITLE_POS		(D3DXVECTOR3(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2 - 80.0f, 0.0f))	// 位置(タイトルロゴ000)
+#define TITLE_SIZE		(D3DXVECTOR2(480.0f, 100.0f))										// サイズ(タイトルロゴ000)
+#define LOGO_POS		(D3DXVECTOR3(TITLE_POS.x + 130.0f, TITLE_POS.y + 100.0f, 0.0f))		// 位置(タイトルロゴ001)
+#define LOGO_SIZE		(D3DXVECTOR2(125.0f, 35.0f))										// サイズ(タイトルロゴ001)
+#define PRESS_POS		(D3DXVECTOR3((SCREEN_WIDTH / 2), (SCREEN_HEIGHT/ 2) + 200.0f, 0.0f))// 位置(Press Start Enter)
+#define PRESS_SIZE		(D3DXVECTOR2(250.0f, 20.0f))										// サイズ(Press Start Enter)
+
+#define TEXT_OBJECTNAME1		"data\\TEXT\\ゲームマップ\\objecy.txt"			// 読み込むテキストファイル
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+//*****************************************************************************
+// 静的メンバ変数
+//*****************************************************************************
+int				CTitle::m_nTitleCounter = 0;		// タイトルのカウンター
+
+CTitleCamera	*CTitle::m_pTitleCamera = NULL;
+bool			CTitle::m_bMenu = false;			// メニュー
+bool			CTitle::m_bOnline = false;			// オンライン
+bool			CTitle::m_bHost = false;			// ホスト
+
+//=============================================================================
+// デフォルトコンストラクタ
+//=============================================================================
+CTitle::CTitle()
+{
+	m_nSetObjectNum = 0;
+
+	for (int nCount = 0; nCount < MAX_MAP_OBJECT; nCount++)
+		m_Map[nCount] = {};
+	
+	m_nCntTex = 0;
+	m_nFrame = 0;
+	m_nLeafposX = 0;
+	m_nLeafsize = 0;
+	m_nCntCreate = 0;
+	m_bOnOff = false;
+	m_nTitleCounter = 0;
+
+	m_bMenu = false;			// メニュー
+	m_bOnline = false;			// オンライン
+	m_bHost = false;			// ホスト
+
+	m_pTitleMenu = NULL;		// タイトルメニュー
+}
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTitle::~CTitle()
+{
+
+}
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CTitle::Init()
+{
+	//===================================
+	//		 Loadの読み込み場所
+	//===================================
+	CFade::Load();		//フェードのテクスチャの読み込み
+	CUi::Load();		//UIのテクスチャの読み込み
+	CTitleMenu::Load();
+	CObject::Load();			//オブジェクトのテクスチャの読み込み
+
+	//マップを読み込む
+	TextLoad(6);
+
+	//	変数の初期化
+	m_nTitleCounter = 0;
+
+	if (m_pTitleCamera == NULL)
+	{
+		m_pTitleCamera = new CTitleCamera;
+		if (m_pTitleCamera != NULL) { m_pTitleCamera->Init(); }
+	}
+
+	// タイトルメニュー
+	if (m_pTitleMenu == NULL)
+	{
+		m_pTitleMenu = CTitleMenu::Create();
+	}
+
+	for (int nCount = 0; nCount < m_nSetObjectNum; nCount++)
+	{
+		//オブジェクトの生成
+		CObject::Create(m_Map[nCount].m_pos, m_Map[nCount].m_rot, m_Map[nCount].m_scale, 0.0f, m_Map[nCount].m_nTexType, m_Map[nCount].m_nType, CModel3D::MOVETYPE_NOT, m_Map[nCount].m_nCollision);
+	}
+
+	CManager::OnlineSeting(false);	// オンライン設定
+
+	return S_OK;
+}
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CTitle::Uninit(void)
+{
+	//===================================
+	//	　　UnLoadの破棄する場所
+	//===================================
+	CFade::UnLoad();	//フェードのテクスチャの破棄
+	CUi::UnLoad();		//UIのテクスチャの破棄
+	CTitleMenu::Unload();
+	CObject::UnLoad();				//オブジェクトのテクスチャの破棄
+
+	// タイトルカメラの破棄
+	if (m_pTitleCamera != NULL)
+	{
+		m_pTitleCamera->Uninit();
+		delete m_pTitleCamera;
+		m_pTitleCamera = NULL;
+	}
+
+	// タイトルメニュー
+	if (m_pTitleMenu != NULL)
+	{
+		m_pTitleMenu->Uninit();
+		m_pTitleMenu = NULL;
+	}
+
+	//フェード以外削除
+	CScene::NotFadeReleseAll();
+}
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTitle::Update(void)
+{
+	//入力情報
+	CInputKeyBoard *pCInputKeyBoard = CManager::GetInput();
+	CInputJoyPad_0 * pXpad = CManager::GetInputJoyPad0(0);					//ジョイパットの取得
+	CInputMouse *pCInputMouse = CManager::GetInputMouse();
+
+	//サウンドの情報
+	CSound *pSound = CManager::GetSound();
+
+	//フェードのポイント
+	CFade::FADE pFade = CFade::GetFade();
+
+	//	タイトルのカウンター加算
+	m_nTitleCounter++;
+
+	if (m_pTitleCamera != NULL) { m_pTitleCamera->Updata(); }
+}
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CTitle::Draw(void)
+{
+	LPDIRECT3DDEVICE9 pDevice = CManager::GetRenderer()->GetDevice();
+
+	// バックバッファ＆Ｚバッファのクリア
+	pDevice->Clear(0,
+		NULL,
+		(D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER),
+		D3DCOLOR_RGBA(157, 184, 224, 255),
+		1.0f,
+		0);
+
+	//タイトルカメラの生成
+	if (m_pTitleCamera != NULL) { m_pTitleCamera->SetCamera(); }
+	
+	//全ての描画
+	CScene::DrawAll();
+}
+
+//===============================================================================
+// ファイルからロード
+//===============================================================================
+void CTitle::TextLoad(int nLoadNumber)
+{
+	//ファイル用変数
+	FILE *pFile;		//ファイルポインタ
+	char *pStrcur;		//現在の先頭の文字列
+	char aLine[256];	//文字列
+	char aStr[256];		//一時保存文字列
+	int nIndex = 0;		//現在のインデックス
+	int nWord = 0;		//ポップで返された値を保持
+
+						//ファイルポインタの初期化処理
+	pFile = NULL;
+
+	//ファイルを開く 読み込み
+	pFile = fopen(TEXT_OBJECTNAME1, "r");
+
+	//NULLチェック
+	if (pFile != NULL)
+	{
+		//文字列の先頭を設定
+		pStrcur = ReadLine(pFile, &aLine[0]);
+		//文字列を取り出す
+		strcpy(aStr, pStrcur);
+
+		//文字列のデータ 比較する文字列 比較する文字数
+		if (memcmp(pStrcur, "OBJECT_SETNUM = ", strlen("OBJECT_SETNUM = ")) == 0)
+		{
+			//頭出し
+			pStrcur += strlen("OBJECT_SETNUM = ");
+			//文字列の先頭を設定
+			strcpy(aStr, pStrcur);
+			//文字列抜き出し
+			m_nSetObjectNum = atoi(pStrcur);
+		}
+
+		//オブジェクトの数分回す
+		for (int nCntObject = 0; nCntObject < m_nSetObjectNum; nCntObject++)
+		{
+			//文字列の先頭を設定
+			pStrcur = ReadLine(pFile, &aLine[0]);
+			//文字列を取り出す
+			strcpy(aStr, pStrcur);
+
+			if (memcmp(pStrcur, "OBJECT_START", strlen("OBJECT_START")) == 0)
+			{
+				while (1)
+				{
+					//文字列の先頭を設定
+					pStrcur = ReadLine(pFile, &aLine[0]);
+
+					//種類のを読み込み
+					if (memcmp(pStrcur, "TYPE = ", strlen("TYPE = ")) == 0)
+					{
+						//頭出し
+						pStrcur += strlen("TYPE = ");
+						//文字列の先頭を設定
+						strcpy(aStr, pStrcur);
+						//文字列抜き出し
+						m_Map[nCntObject].m_nType = atoi(pStrcur);
+					}
+					//種類のを読み込み
+					if (memcmp(pStrcur, "TEXTURETYPE = ", strlen("TEXTURETYPE = ")) == 0)
+					{
+						//頭出し
+						pStrcur += strlen("TEXTURETYPE = ");
+						//文字列の先頭を設定
+						strcpy(aStr, pStrcur);
+						//文字列抜き出し
+						m_Map[nCntObject].m_nTexType = atoi(pStrcur);
+					}
+					//当たり判定を読み込み
+					if (memcmp(pStrcur, "COLLISION = ", strlen("COLLISION = ")) == 0)
+					{
+						//頭出し
+						pStrcur += strlen("COLLISION = ");
+						//文字列の先頭を設定
+						strcpy(aStr, pStrcur);
+						//文字列抜き出し
+						m_Map[nCntObject].m_nCollision = atoi(pStrcur);
+					}
+					//POSを読み込み
+					if (memcmp(pStrcur, "POS = ", strlen("POS = ")) == 0)
+					{
+						//頭出し
+						pStrcur += strlen("POS = ");
+						//文字列の先頭を設定
+						strcpy(aStr, pStrcur);
+
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_pos.x = (float)atof(pStrcur);
+						//文字数分進める
+						pStrcur += nWord;
+
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_pos.y = (float)atof(pStrcur);
+						//文字数分進める
+						pStrcur += nWord;
+
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_pos.z = (float)atof(pStrcur);
+
+					}
+					//ROTを読み込み
+					if (memcmp(pStrcur, "ROT = ", strlen("ROT = ")) == 0)
+					{
+						//頭出し
+						pStrcur += strlen("ROT = ");
+						//文字列の先頭を設定
+						strcpy(aStr, pStrcur);
+
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_rot.x = (float)atof(pStrcur);
+
+						//文字数分進める
+						pStrcur += nWord;
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_rot.y = (float)atof(pStrcur);
+
+						//文字数分進める
+						pStrcur += nWord;
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_rot.z = (float)atof(pStrcur);
+					}
+					//SIZを読み込み
+					if (memcmp(pStrcur, "SIZ = ", strlen("SIZ = ")) == 0)
+					{
+						//頭出し
+						pStrcur += strlen("SIZ = ");
+						//文字列の先頭を設定
+						strcpy(aStr, pStrcur);
+
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_scale.x = (float)atof(pStrcur);
+
+						//文字数分進める
+						pStrcur += nWord;
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_scale.y = (float)atof(pStrcur);
+
+						//文字数分進める
+						pStrcur += nWord;
+						//文字数を返してもらう
+						nWord = PopString(pStrcur, &aStr[0]);
+						//文字列変換
+						m_Map[nCntObject].m_scale.z = (float)atof(pStrcur);
+					}
+					else if (memcmp(pStrcur, "OBJECT_END", strlen("OBJECT_END")) == 0)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+//=============================================================================
+//　ファイル読み込み無効文を排除
+//=============================================================================
+char *CTitle::ReadLine(FILE *pFile, char *pDst)
+{
+	while (1)
+	{
+		//１行分読み込み
+		fgets(&pDst[0], 256, pFile);
+
+		//文字列のデータ 比較する文字列 比較する文字数
+		if (memcmp(pDst, "#", strlen("#")) == 0)
+		{
+			pDst += strlen("\n");
+		}
+		//文字列のデータ 比較する文字列 比較する文字数
+		else if (memcmp(pDst, "\t", strlen("\t")) == 0)
+		{
+			pDst += strlen("\t");
+			while (1)
+			{
+				if (memcmp(pDst, "\t", strlen("\t")) == 0)
+				{
+					pDst += strlen("\t");
+				}
+				else
+				{
+					break;
+				}
+			}
+			break;
+		}
+		//文字列のデータ 比較する文字列 比較する文字数
+		else if (memcmp(pDst, " ", strlen(" ")) == 0)
+		{
+			pDst += strlen(" ");
+			while (1)
+			{
+				if (memcmp(pDst, " ", strlen(" ")) == 0)
+				{
+					pDst += strlen(" ");
+				}
+				else
+				{
+					break;
+				}
+				break;
+			}
+		}
+		//文字列のデータ 比較する文字列 比較する文字数
+		else if (memcmp(pDst, "\n", strlen("\n")) == 0)
+		{
+			pDst += strlen("\n");
+		}
+		else
+		{
+			break;
+		}
+	}
+	return pDst;
+}
+
+//=============================================================================
+//　ファイル読み込み先頭を排除
+//=============================================================================
+char *CTitle::GetLineTop(char * pStr)
+{
+	while (1)
+	{
+		//文字列のデータ 比較する文字列 比較する文字数
+		if (memcmp(pStr, " ", strlen(" ")) == 0)
+		{
+			pStr += strlen(" ");
+			while (1)
+			{
+				if (memcmp(pStr, " ", strlen(" ")) == 0)
+				{
+					pStr += strlen(" ");
+				}
+				else
+				{
+					break;
+				}
+				break;
+			}
+		}
+		//文字列のデータ 比較する文字列 比較する文字数
+		else if (memcmp(pStr, "\t", strlen("\t")) == 0)
+		{
+			pStr += strlen("\t");
+			while (1)
+			{
+				if (memcmp(pStr, "\t", strlen("\t")) == 0)
+				{
+					pStr += strlen("\t");
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	return pStr;
+}
+
+//=============================================================================
+//　文字数を返す
+//=============================================================================
+int CTitle::PopString(char * pStr, char * pDest)
+{
+	int nWord = 0;
+
+	while (1)
+	{	//頭出し
+		pStr += 1;
+		nWord += 1;
+		if (memcmp(pStr, " ", strlen(" ")) == 0)
+		{	//頭出し
+			pStr = "\0";
+			nWord += 1;
+			break;
+		}
+		if (memcmp(pStr, "\t", strlen("\t")) == 0)
+		{	//頭出し
+			pStr = "\0";
+			nWord += (int)strlen("\t");
+			break;
+		}
+		//文字列のデータ 比較する文字列 比較する文字数
+		else if (memcmp(pStr, "\n", strlen("\n")) == 0)
+		{
+			//頭出し
+			nWord += (int)strlen("\n");
+			break;
+		}
+	}
+	strcpy(pDest, pStr);
+	//文字列の数を返す
+	return nWord;
+}
---
+++ BaseProject/ChickenRace/ChickenRace/title.h
@@ -0,0 +1,92 @@
+//=============================================================================
+//
+// メイン処理 [title.h]
+// Author : 有馬 武志
+//
+//=============================================================================
+#ifndef _TITLE_H_
+#define _TITLE_H_
+
+#include "main.h"
+#include "sound.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define MAX_MAP_OBJECT		(3500)
+
+//*****************************************************************************
+// 前方宣言
+//*****************************************************************************
+class CTitleCamera;
+
+class CTitleMenu;
+
+//=====================
+// 基本クラス
+//=====================
+class CTitle
+{
+public:
+	CTitle();								// コンストラクタ
+	~CTitle();								// デストラクタ
+	HRESULT Init();							// 初期化処理
+	void	Uninit(void);					// 終了処理
+	void	Update(void);					// 更新処理
+	static void	Draw(void);					// 描画処理
+
+	static int GetCounter(void) { return m_nTitleCounter; }
+
+	// メニュー
+	static void SetMenu(bool bMenu)		{ m_bMenu = bMenu; }
+	static bool GetMenu(void)			{ return m_bMenu; }
+	// オンライン
+	static void SetOnline(bool bOnline) { m_bOnline = bOnline; }
+	static bool GetOnline(void)			{ return m_bOnline; }
+	// ホスト
+	static void SetHost(bool bHost)		{ m_bHost = bHost; }
+	static bool GetHost(void)			{ return m_bHost; }
+
+	static CTitleCamera * GetTitleCamera(void) { return m_pTitleCamera; }
+
+private:
+	typedef struct
+	{
+		int					m_nType;			// 種類
+		int					m_nTexType;			// テクスチャの種類
+		int					m_nCollision;		// 当たり判定のONOFF
+		D3DXVECTOR3			m_pos;				// 位置
+		D3DXVECTOR3			m_rot;				// 回転
+		D3DXVECTOR3			m_scale;			// サイズ
+	}Map;
+
+	//---------------------------------
+	//ファイルを読み込む
+	//---------------------------------
+	void TextLoad(int nLoadNumber);
+
+	char *ReadLine(FILE *pFile, char *pDst);	//1行読み込み
+	char *GetLineTop(char *pStr);				//行の先頭を取得
+	int  PopString(char *pStr, char *pDest);	//行の最後を切り捨て
+
+	int m_nSetObjectNum;						//オブジェクトを置いた数
+
+	Map m_Map[MAX_MAP_OBJECT];					//設置するオブジェクトの構造体
+
+	int					m_nFrame;			// 季節のカウンター
+	int					m_nCntTex;			// テクスチャのカウンター
+	int					m_nCntCreate;		// 生成時間
+	int					m_nLeafposX;		// 落ち葉の 生成座標
+	int					m_nLeafsize;		// 落ち葉の サイズ
+	static int			m_nTitleCounter;	// タイトルのカウンター
+	bool				m_bOnOff;
+
+	static CTitleCamera	*m_pTitleCamera;	//タイトルカメラのポインタ
+
+	static bool			m_bMenu;			// メニュー
+	static bool			m_bOnline;			// オンライン
+	static bool			m_bHost;			// ホスト
+
+	CTitleMenu			*m_pTitleMenu;		// タイトルメニュー
+};
+#endif
